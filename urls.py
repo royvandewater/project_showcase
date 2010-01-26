08eead6475d3180894e0addbfe4caf3d68613ef7
Roy van de Water <roy.v.water@gmail.com>
2010-01-26 16:18:19 -0700

    Set / to load news view

--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^project_showcase/', include('project_showcase.foo.urls')),
+    (r'^$', 'news.views.news'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
