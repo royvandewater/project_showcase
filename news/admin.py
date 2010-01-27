e16cad41d4bce5a5402301b653dfa831e60bbd6c
Roy van de Water <roy.v.water@gmail.com>
2010-01-26 20:02:33 -0700

    enabled the admin section

---
+++ news/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from models import *
+
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Article, ArticleAdmin)
--- news/views.py
+++ news/views.py
@@ -4,4 +4,5 @@ from django.template import RequestContext
 from models import *
 
 def news(request):
+    articles = Article.objects.order_by('publish_date')[:10]
     return render_to_response('news.html', locals(), context_instance=RequestContext(request))
