e8505dc5b96baa5202d4bf30779877de24de880e
Roy van de Water <roy.v.water@gmail.com>
2010-01-29 23:55:36 -0700

    view calls the news content object

--- news/views.py
+++ news/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from models import *
+from main.models import Content
 
 def news(request):
+    content = Content.bodels.objects.get(name='news') 
     articles = Article.objects.order_by('publish_date')[:10]
     return render_to_response('news/news.html', locals(), context_instance=RequestContext(request))
