536370ff8551543491e0d8ea09e9f909066d2570
Roy van de Water <roy.v.water@gmail.com>
2010-01-31 18:26:55 -0700

    Removed line of songfile that accidently got in from partybeat

--- news/views.py
+++ news/views.py
@@ -7,5 +7,4 @@ from main.models import Content
 def news(request):
     content = Content.objects.get(name='news') 
     articles = Article.objects.order_by('publish_date')[:10]
-    artists = SongFile.objects.all().values_list('artist', flat=True)
     return render_to_response('news/news.html', locals(), context_instance=RequestContext(request))
