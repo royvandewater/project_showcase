2158dfe3604b9b90fcdb46a256d9b271416ec90c
Roy van de Water <roy.v.water@gmail.com>
2010-02-03 23:48:27 -0700

    Added a full res field to screenshot model

--- screenshots/models.py
+++ screenshots/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Screenshot(models.Model):
     title = models.CharField(max_length=100)
     version = models.IntegerField()
-    thumbnail = models.ImageField(upload_to="upload/screenshots")
+    thumbnail = models.ImageField(upload_to="upload/screenshots/thumb/")
+    full = models.ImageField(upload_to="upload/screenshots/full/")
 
     def __unicode__(self):
         return self.title
