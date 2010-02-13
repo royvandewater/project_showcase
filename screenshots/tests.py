6627c7a53e8f07c276ca218fd6f006609bc825fb
Roy van de Water <roy.v.water@gmail.com>
2010-02-12 19:04:17 -0700

    Some more in-depth testing

--- screenshots/tests.py
+++ screenshots/tests.py
@@ -56,9 +56,10 @@ class SimpleTest(TestCase):
         Tests that the screenshots view returns code 200
         """
         self.assertContains(self.client.get("/screenshots/"), "<h1>Screenshots</h1>", status_code=200)
+        self.assertContains(self.client.get("/screenshots/"), "<h2>v0.5b</h2>", status_code=200)
 
     def test_view_past_version(self):
         """
         Tests that the past_versions view returns code 200
         """
-        self.check_response_code("/screenshots/0.3a/", 200)
+        self.assertContains(self.client.get("/screenshots/0.3a/"), "<h2>v0.3a</h2>", status_code=200)
