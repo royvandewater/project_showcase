7d60b5e56269553d5ed93674101f7834a58b665b
Roy van de Water <roy.v.water@gmail.com>
2010-02-13 20:30:05 -0700

    Added basic register template

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 
 class UserTests(TestCase):
+    fixtures = ['testdata']
 
     def test_view_new(self):
         """
         Tests the user registration view
         """
-        self.assertContains(self.client.get("/users/new/"), "<h2>Register</h2>", status_code=200)
+        self.assertContains(self.client.get("/users/new/"), "<h1>Register</h1>", status_code=200)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 # from django.shortcuts import render_to_response
 # from django.template import RequestContext
 
+from main.models import Content
+
 def new(request):
-    return HttpResponse()
+    content = Content.objects.get(name='register')
+    return render_to_response('users/new.html', locals(), context_instance=RequestContext(request))
