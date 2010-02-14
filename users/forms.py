89e578d7d9c0b1e29e9d5619559e51dd1fd1d417
Roy van de Water <roy.v.water@gmail.com>
2010-02-14 14:58:58 -0700

    Changed registration fields to match those from users.auth module

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 class RegisterForm(forms.Form):
     email = forms.EmailField(required=True, label="Email address")
-    display_name = forms.CharField(max_length=255, required=True, label="Display Name")
+    username = forms.CharField(max_length=255, required=True, label="Display Name")
+    first_name = forms.CharField(max_length=255, required=True, label="First Name")
+    last_name = forms.CharField(max_length=255, required=True, label="Last Name")
     password = forms.CharField(max_length=255, required=True, widget=forms.PasswordInput, label="Password")
     confirm_password = forms.CharField(max_length=255, required=True, widget=forms.PasswordInput, label="Confirm Password")
 
