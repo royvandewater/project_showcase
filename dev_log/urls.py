77bf7df94cab1c5c9378f4e25de8cb3fe0622921
Roy van de Water <roy.v.water@gmail.com>
2010-02-25 18:47:44 -0700

    Revert "Allowing any character in git secret key"
    This reverts commit 01d2ecb17d88205465b2427e78e25170e7b4cd6d.

--- dev_log/urls.py
+++ dev_log/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_detail
 
 urlpatterns = patterns('dev_log.views',
-    (r'github/(?P<git_key>.+)/$', 'github'),
+    (r'github/(?P<git_key>[a-zA-Z0-9]+)/$', 'github'),
     (r'^$', 'index'),
 )
